542ffeb4e9f9c05e8f014817f1545bbd041c82d2
travisen <travis.nykaza@gmail.com>
2017-04-25 13:46:24 -0400

    Add game and mouse files

---
+++ game/game.lua
@@ -0,0 +1,14 @@
+Game = Object:extend()
+
+function Game:new()
+  require "classes/mouse.lua"
+end
+
+function Game:update(dt)  
+
+end
+
+
+function Game:draw()
+  
+end
---
+++ main.lua
@@ -0,0 +1,18 @@
+DEBUGGING = true -- true to turn debugging on.
+
+function love.load()
+  Object = require "libs/classic"
+  require "game/game"
+
+  
+end
+ 
+-- Update game objects
+function love.update(dt)
+
+end
+ 
+-- Draw a coloured rectangle.
+function love.draw()
+
+end
